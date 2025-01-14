bb8f15f751d6dd2a74bedd2a7eea371fc7f67385
Lens <yoanyombapro@gmail.com>
2025-01-14 01:47:54 -0500

    chore: add storybook
    Signed-off-by: Lens <yoanyombapro@gmail.com>

---
+++ clients/dub/packages/ui/.storybook/preview.ts
@@ -0,0 +1,30 @@
+import '../src/styles/globals.css'
+
+import type { Preview } from '@storybook/react'
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    layout: 'centered',
+  },
+  globalTypes: {
+    theme: {
+      name: 'Theme',
+      description: 'Global theme for components',
+      defaultValue: 'light',
+      toolbar: {
+        icon: 'circlehollow',
+        items: ['light', 'dark'],
+        showName: true,
+      },
+    },
+  },
+}
+
+export default preview
--- clients/dub/packages/ui/tsup.config.ts
+++ clients/dub/packages/ui/tsup.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig, Options } from "tsup";
+import { type Options, defineConfig } from "tsup";
 
 export default defineConfig((options: Options) => ({
   entry: {
     index: "src/index.tsx",
     "icons/index": "src/icons/index.tsx",
     "charts/index": "src/charts/index.ts",
+    "ui/index": "src/ui/index.tsx",
   },
-
+  target: "node18",
   format: ["esm"],
   esbuildOptions(options) {
     options.banner = {
